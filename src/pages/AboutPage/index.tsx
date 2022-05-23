0ee485e10dc9b9b2259806ae3e1497a291f50464
ninja <ersanyakit@yahoo.com.tr>
2022-05-23 10:18:27 +0300

    statusbar Mobile issues fixed

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -7,10 +7,40 @@ const AboutPage: React.FunctionComponent<IPage> = props => {
         document.title = props.title + " - Terra $LUNA Burn Machine";
     },[]);
     return(
-        <div>
-            <h1>About Page</h1>
-            <p>Under of development</p>
+        <div className={"form-bg"}>
+        <div className={"row"}>
+            <div className={"col-12 text-center"}>
+                <h2>Save Terra $LUNA</h2>
+                <hr/>
+            </div>
+            <div className={"col-lg-6 col-sm-12"}>
+                    <h4>TerraLunaStats</h4>
+                    <ul>
+                        <li>Telegram</li>
+                        <ol><a target={"_blank"} href={"https://t.me/terralunastats"}>Join our telegram group</a></ol>
+                        <li>Source Code</li>
+                        <ol><a target={"_blank"}  href={"https://github.com/ersanyakit/terralunastats"}>Source codes of terralunastats.com</a></ol>
+                        <li>Feature Requests</li>
+                        <ol><a target={"_blank"}  href={"https://github.com/ersanyakit/terralunastats/issues"}>Click here</a></ol>
+                        <li>Follow Developer</li>
+                        <ol><a target={"_blank"}  href={"https://twitter.com/ersanyakit"}>ersan.eth</a></ol>
+                    </ul>
+            </div>
+
+            <div className={"col-lg-6 col-sm-12"}>
+                <h4>Special Thanks Goes To</h4>
+                <ul>
+                    <li><a target={"_blank"}  href={"https://twitter.com/Dnasc4"}>Dnasc</a></li>
+                    <li>Ambroise H(@makrbuildr)</li>
+                    <li>Terra Agora Rehabilitation</li>
+                    <ol><a target={"_blank"} href={"https://t.me/TerraAgora"}>Join Terra Agora group</a></ol>
+                    <li><a target={"_blank"}  href={"https://twitter.com/martkist"}>Martkist</a></li>
+                </ul>
+            </div>
+
         </div>
+        </div>
+
     )
 }
 
